be930fe3de959e0bc90f01c71e1e6f61cd886881
John Daryll Sampilingan <johndaryllramos8@gmail.com>
2025-01-23 16:56:01 +0800

    add logos

---
+++ pop-upcontainer.js
@@ -0,0 +1,22 @@
+function showPopup(imageUrls) {
+    const popup = document.getElementById('popup');
+    const imagesContainer = document.getElementById('popup-images');
+
+    // Clear existing images
+    imagesContainer.innerHTML = '';
+
+    // Add new images
+    imageUrls.forEach(url => {
+        const img = document.createElement('img');
+        img.src = url;
+        img.alt = 'Popup Image';
+        imagesContainer.appendChild(img);
+    });
+
+    popup.style.display = 'block';
+}
+
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
